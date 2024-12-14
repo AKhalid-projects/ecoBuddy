1f1378d9f0cf0c8e3885bbae65b53550ddcbdaa4
ak <ak.personalprojects@gmail.com>
2024-12-14 11:58:48 +0300

    checking eco facilities

--- test_sqlite.php
+++ test_sqlite.php
@@ -1,7 +1,7 @@
 <?php
 $dbPath = __DIR__ . '/database/ecobuddy.db';
 $pdo = new PDO("sqlite:$dbPath");
-$stmt = $pdo->query("SELECT * FROM users");
-$users = $stmt->fetchAll(PDO::FETCH_ASSOC);
-echo "<pre>" . print_r($users, true) . "</pre>";
+$stmt = $pdo->query("SELECT * FROM eco_facilities");
+$facilities = $stmt->fetchAll(PDO::FETCH_ASSOC);
+echo "<pre>" . print_r($facilities, true) . "</pre>";
 ?>
